1a62125518be5c401b588ac0119d41ff875645bc
Manuel Rodrigues <mr.1311223@gmail.com>
2020-09-16 07:49:50 -0400

    Add Badges List

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
 const container = document.getElementById("app");
 
 const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
