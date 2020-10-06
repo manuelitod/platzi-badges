ca1a97da570ba05adc2842d86b5c77565070a3f1
Manuel Rodrigues <mr.1311223@gmail.com>
2020-10-06 12:44:58 +0100

    Add Badge details page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import BadgeNew from "../pages/BadgeNew";
 import BadgeEdit from "../pages/BadgeEdit";
+import BadgeDetails from "../pages/BadgeDetails";
 import Badges from "../pages/Badges";
 import Layout from "./Layout";
 import NotFound from "../pages/NotFound";
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route exact path="/" component={Home} />
           <Route component={NotFound} />
         </Switch>
